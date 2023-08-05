fb4c04115bd38a0928245f6b600caa44e7ac5ffd
aristides1000 <aristides1000@gmail.com>
2023-08-05 11:20:07 -0400

    Finish add implemantation

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("#toggle-btn").click(function () {
+    // change background color
+    $("body").toggleClass("change-background");
+    $(this).css("border", "1px solid #f5f5f5");
+
+    // toggle buttom text
+    $("span").text(
+      $("span").text() == "Light Mode" ? "Dark Mode"
+      : "Light Mode"
+    );
+  });
+  $("#toggle-btn").click(function() {
+    let icon = $(this).find("ion-icon");
+    icon.attr("name") == "moon" ? icon.attr("name", "sunny") : icon.attr("name", "moon");
+  })
+});
